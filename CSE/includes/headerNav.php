88061177490b1b11a1f692ba05d0481c8c13d953
ARUNTEJAMENDA <aruntejamenda@gmail.com>
2024-03-13 18:17:47 +0530

    cse site

---
+++ CSE/includes/headerNav.php
@@ -0,0 +1,22 @@
+<header>
+    <img id="logo" src="assets/img/cse_logo.jpg" alt="logo" />
+    <div id="head">
+        <p id="headTitle">Department of Computer Science & Engineering</p>
+        <p id="headDesc">Rajiv Gandhi University of Knowledge Technologies - Nuzvid, Eluru District, A.P, India.</p>
+    </div>
+    <div id="others">
+        <p><a href="#">A - Z Index</a> | <a href="#">Sitemap</a></p>
+    </div>
+</header>
+<nav>
+    <ol>
+        <a href="#"><li>Home</li></a>
+        <a href="#"><li>People</li></a>
+        <a href="#"><li>Research</li></a>
+        <a href="#"><li>Academics</li></a>
+        <a href="#"><li>Events</li></a>
+        <a href="#"><li>Resources</li></a>
+        <a href="#"><li>About Us</li></a>
+        <a href="#"><li>Contact Us</li></a>
+    </ol>
+</nav>
