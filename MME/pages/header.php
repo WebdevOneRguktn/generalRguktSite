dd1f2e534f8d525798aee3f9e7a949166a75f52e
HarithaWebDeveloper <webdev.2.rguktn@gmail.com>
2024-03-13 17:49:31 +0530

    commit

---
+++ MME/pages/header.php
@@ -0,0 +1,49 @@
+
+<div class="navbar-top-container">
+      <p>director@rguktn.ac.in</p>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white navbar-container pl-5 pr-5">
+        <div class="footer-logo md-text-center" style="display:flex;margin-left: 20px;">
+            <a href="#"><img src="./images/logo.jpg" alt="" style="width: 68px;text-align: center;"></a>&nbsp;&nbsp;
+               <h5 class="fontlight" style="padding-top: 10px;font-size: 25px; margin-bottom: 0px;color:maroon;font-weight: 500;">Metallurgical and <br>Materials Engineering<p class="fontlight" style="font-size: 16px;color:#000 !important;">	IIIT Nuzvid</p>
+            </h5>
+        </div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./academics.php">Academics</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./people.php">People</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./research.php">Research</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./industry.php">Industry</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./newsandevents.php">News&Events</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="./contact.php">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
