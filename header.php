e6c08b385446bef253a75cca5a5262285a5295a9
HavanthiRoyal <webdev.9.rguktn@gmail.com>
2024-03-14 19:25:37 +0530

    uploading

---
+++ header.php
@@ -0,0 +1,99 @@
+<section class="ftco-section ">
+            <div class="container-fluid px-md-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="row" style="padding: 15px 0px 0px 0px;">
+                            <div class="col-md-2 iitm-logo">
+                                <img src="./Mechanical Engineering_files/images/rgukt-img.png" class="site_logo mr-4">
+                            </div>
+                            <div class="col-md-6 dept-name" style="padding: 10px 0px 0px 0px;">
+                                <a class="navbar-brand" href="https://mech.iitm.ac.in/index.php"> DEPARTMENT OF MECHANICAL ENGINEERING <span style="padding: 10px 0px 0px 0px; font-size: 15px;">
+                                    Rajiv Gandhi university of Knowledge and Technologies</span>
+                                </a>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </section>  
+
+<!--------Navbar----->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+		
+		  
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+			<a class="nav-link text-dark" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				HOME
+			  </a></li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			PEOPLE
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+			<a class="dropdown-item" href="#">Faculty</a>
+  			<a class="dropdown-item" href="#">Distinguished Alumini Awardees</a>
+  			<a class="dropdown-item" href="#">Visitors</a>
+			  <a class="dropdown-item" href="#">Student Representatives</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			RESEARCH
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">Research Focus</a>
+  			<a class="dropdown-item" href="#">Laboratories</a>
+  			<a class="dropdown-item" href="#">Graduate Research</a>
+			<a class="dropdown-item" href="#">Collaborations</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			ACADEMICS
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
+			<a class="dropdown-item" href="#">Overview</a>
+  			<a class="dropdown-item" href="#">curriculum</a>
+  			<a class="dropdown-item" href="#">B.Tech/M.Tech</a>
+			  <a class="dropdown-item" href="#"></a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			EVENTS
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown4">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">Conferences/Workshops</a>
+  			<a class="dropdown-item" href="#">Short Term Courses</a>
+  			<a class="dropdown-item" href="#">---</a>
+			  <a class="dropdown-item" href="#">----</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="dropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			FACILITIES
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">---</a>
+			<a class="dropdown-item" href="#">----</a>
+			<a class="dropdown-item" href="#">---</a>
+			<a class="dropdown-item" href="#">----</a>
+		  
+		  </div>
+		</li>
+		
+	  </ul>
+	</div>
+  </nav>
