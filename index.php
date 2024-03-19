867196789aca011213cc2183440b1762f870bb85
HavanthiRoyal <webdev.9.rguktn@gmail.com>
2024-03-19 11:07:17 +0530

    changed

--- index.php
+++ index.php
@@ -8,12 +8,118 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="./Mechanical Engineering_files/css/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	 
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body>
 <!-------------------------------------------------  header -------------------------------------------->
-<?php include "header.php"?>
+<section class="ftco-section ">
+            <div class="container-fluid px-md-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="row" style="padding: 15px 0px 0px 0px;">
+                            <div class="col-md-2 rgukt-logo">
+                                <img src="./Mechanical Engineering_files/images/rgukt-img.png" class="site_logo mr-4">
+                            </div>
+                            <div class="col-md-6 dept-name" style="padding: 10px 0px 0px 0px;">
+                                <a class="navbar-brand" href="#"> DEPARTMENT OF MECHANICAL ENGINEERING <span style="padding: 10px 0px 0px 0px; font-size: 15px;">
+                                    Rajiv Gandhi university of Knowledge and Technologies</span>
+                                </a>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </section>  
+
+<!--------Navbar----->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+		
+		  
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+			<a class="nav-link text-dark" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				HOME
+			  </a></li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			PEOPLE
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+			<a class="dropdown-item" href="./faculty_page.php">Faculty</a>
+			<a class="dropdown-item" href="./staff.php">Staff</a>
+  			<a class="dropdown-item" href="#">Distinguished Alumini Awardees</a>
+  			<a class="dropdown-item" href="#">Visitors</a>
+			  <a class="dropdown-item" href="#">Student Representatives</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			RESEARCH
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">Research Focus</a>
+  			<a class="dropdown-item" href="#">Laboratories</a>
+  			<a class="dropdown-item" href="#">Graduate Research</a>
+			<a class="dropdown-item" href="#">Collaborations</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			ACADEMICS
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
+			<a class="dropdown-item" href="#">Overview</a>
+  			<a class="dropdown-item" href="#">curriculum</a>
+  			<a class="dropdown-item" href="#">B.Tech/M.Tech</a>
+			  <a class="dropdown-item" href="#"></a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			EVENTS
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="navbarDropdown4">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">Conferences/Workshops</a>
+  			<a class="dropdown-item" href="#">Short Term Courses</a>
+  			<a class="dropdown-item" href="#">---</a>
+			  <a class="dropdown-item" href="#">----</a>
+		  </div>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-dark" href="#" id="dropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			FACILITIES
+		  </a>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<!-- Submenu items go here -->
+			<a class="dropdown-item" href="#">---</a>
+			<a class="dropdown-item" href="#">----</a>
+			<a class="dropdown-item" href="#">---</a>
+			<a class="dropdown-item" href="#">----</a>
+		  
+		  </div>
+		</li>
+		
+	  </ul>
+	</div>
+  </nav>
+<!------------------------------------------------------------------------->
 	<section class="fullsize-video-bg">
 		<div class="video-sec">
 			<div id="video-viewport">
